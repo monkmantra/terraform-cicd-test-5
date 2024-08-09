f5b008fa6157a9fb7496efb6c53d392e0901c6c2
Jinal Desai <58817990+jinaldesailive@users.noreply.github.com>
2024-08-09 14:47:39 +0100

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,13 +1,13 @@
-resource "google_storage_bucket" "png-gcp-learning-poc" {
-  name          = "png-gcp-learning-poc-111"
-  location      = "US"
-  force_destroy = true
-}
+#resource "google_storage_bucket" "png-gcp-learning-poc" {
+#  name          = "png-gcp-learning-poc-111"
+#  location      = "US"
+#  force_destroy = true
+#}
 
-resource "google_bigquery_dataset" "test123_bigquery_dataset" {
-  dataset_id    = "test123"
-  friendly_name = "test123"
-  description   = "test123 dataset"
-  location      = "us-east4"
-  project       = "png-gcp-learning-poc"
-}
+#resource "google_bigquery_dataset" "test123_bigquery_dataset" {
+#  dataset_id    = "test123"
+#  friendly_name = "test123"
+#  description   = "test123 dataset"
+#  location      = "us-east4"
+#  project       = "png-gcp-learning-poc"
+#}
